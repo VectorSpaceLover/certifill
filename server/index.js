58b52bc0fffe0c2ac07ce937f70b235b82e7720b
venus9023 <venus9023gold@mail.ru>
2022-04-08 15:05:58 +0900

    backend-1

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+const e = require("express");
+
+var app = express();
+app.use(bodyParser({ limit: "50mb" }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require("./routes/user.js")(app);
+// require("./routes/item.js")(app);
+
+app.listen(5000, () => console.log("Server @ port 5000"));
+module.exports = app;
+
+
